2f0cb1fff0463c58b448e8957c1e55fe6c9e141e
toeknee919 <tony.hess@me.com>
2015-01-22 22:23:27 -0800

    added styling, mobil, multiline query

--- js/app.js
+++ js/app.js
@@ -14,10 +14,15 @@ $(document).ready(function() {
 	});
 });
 
+
+//********************************************************************
 //populates the original results on the page
+//********************************************************************
 function populate_results(word, n) {
 
-	var url = "http://www.en.wikipedia.org/w/api.php?format=json&action=query&titles="+word+"&prop=revisions&rvprop=content&aifrom="+word+"&list=allimages&ailimit="+n+"aisort=name&callback=?";
+	//makes query url friendly for >1 words
+	tword = word.replace(/\s/g, "+");
+	var url = "http://www.en.wikipedia.org/w/api.php?format=json&action=query&titles="+tword+"&prop=revisions&rvprop=content&aifrom="+tword+"&list=allimages&ailimit="+n+"aisort=name&callback=?";
 	$.getJSON( url, function( data ) {
 	var a = $.parseJSON(JSON.stringify(data));
 
@@ -42,14 +47,17 @@ function populate_results(word, n) {
 			    //console.log(a.query);
 			};
 			//update the search more button
-			update_search_button(word, n);
+			update_search_button(tword, n);
 
 		});
 	//clears the search box
-	$('#searchfield').val(''); 
+	//$('#searchfield').val(''); 
 }
 
-//creates a button to search from n to n+25 to add results to the page
+
+//********************************************************************
+//creates a button to search from n to n+25 to add additional results to the page
+//********************************************************************
 function update_search_button(word, n){
 	var a = parseInt(n) + 25;
 	$("#more").empty();
@@ -59,7 +67,11 @@ function update_search_button(word, n){
     
 }
 
-// adds more results to the page (n contains the number of results the page will now contain)
+
+
+//******************************************************************************************
+// adds more results to the page (n-1 = the number of results the page will now contain)
+//******************************************************************************************
 function add_more_results(n, word){
 
 	var url = "http://www.en.wikipedia.org/w/api.php?format=json&action=query&titles="+word+"&prop=revisions&rvprop=content&aifrom="+word+"&list=allimages&ailimit="+n+"aisort=name&callback=?";
@@ -88,7 +100,4 @@ function add_more_results(n, word){
 			update_search_button(word, n);
 
 		});
-	//clears the search box
-
-	$('#searchfield').val(''); 
 }
